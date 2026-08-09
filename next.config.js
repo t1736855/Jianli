1486e71ce460ff44826063febe1fdd52510c2bee
T1736855 <t1736855@gmail.com>
2026-08-09 14:59:34 +0800

    静态导出

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  basePath: '/jianli',
   images: {
-    domains: [],
+    unoptimized: true,
   },
 };
 
